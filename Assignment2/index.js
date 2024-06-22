ec32bb2116ca1de63524ca79559b90e8bf10f352
kiran4221 <155857095+kiran4221@users.noreply.github.com>
2024-06-21 17:46:01 -0700

    Update index.js

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -36,33 +36,6 @@ class DoublyLinkedList {
         console.log(str + 'null');
     }
 
-    printForwardWithRecursion(node) {
-        if (!node) {
-            console.log('null');
-            return;
-        }
-        process.stdout.write(node.data + ' -> ');
-        this.printForwardWithRecursion(node.next);
-    }
-
-    printBackwardsWithRecursion(node) {
-        if (!node) {
-            return;
-        }
-        this.printBackwardsWithRecursion(node.next);
-        process.stdout.write(node.data + ' -> ');
-    }
-
-    printBackwardsWithWhile() {
-        let current = this.tail;
-        let str = 'null';
-        while (current) {
-            str = ' -> ' + current.data + str;
-            current = current.prev;
-        }
-        console.log('null' + str);
-    }
-
     removeAt(position) {
         if (position < 0 || position >= this.size) {
             return false;
@@ -98,81 +71,15 @@ class DoublyLinkedList {
         return false;
     }
 
-    updateAt(position, newData) {
-        let current = this.head;
-        let index = 0;
-        while (current) {
-            if (index === position) {
-                current.data = newData;
-                return true;
-            }
-            current = current.next;
-            index++;
-        }
-        return false;
-    }
-
-    search(data) {
-        let current = this.head;
-        while (current) {
-            if (current.data === data) {
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    searchWithRecursion(data, node) {
-        if (!node) {
-            return false;
-        }
-        if (node.data === data) {
-            return true;
-        }
-        return this.searchWithRecursion(data, node.next);
-    }
-
-    static mergeTwoSortedLists(list1, list2) {
-        const mergedList = new DoublyLinkedList();
-        let current1 = list1.head;
-        let current2 = list2.head;
-
-        while (current1 && current2) {
-            if (current1.data < current2.data) {
-                mergedList.append(current1.data);
-                current1 = current1.next;
-            } else {
-                mergedList.append(current2.data);
-                current2 = current2.next;
-            }
-        }
-
-        while (current1) {
-            mergedList.append(current1.data);
-            current1 = current1.next;
-        }
-
-        while (current2) {
-            mergedList.append(current2.data);
-            current2 = current2.next;
-        }
-
-        return mergedList;
-    }
-
     deleteNthFromEnd(n) {
         if (n <= 0 || n > this.size) {
             return false;
         }
-
         let fast = this.head;
         let slow = this.head;
-
         for (let i = 0; i < n; i++) {
             fast = fast.next;
         }
-
         if (!fast) {
             this.head = this.head.next;
             if (this.head) {
@@ -183,25 +90,46 @@ class DoublyLinkedList {
             this.size--;
             return true;
         }
-
         while (fast.next) {
             fast = fast.next;
             slow = slow.next;
         }
-
         slow.next = slow.next.next;
         if (slow.next) {
             slow.next.prev = slow;
         } else {
             this.tail = slow;
         }
-
         this.size--;
         return true;
     }
+
+    static mergeTwoSortedLists(list1, list2) {
+        const mergedList = new DoublyLinkedList();
+        let current1 = list1.head;
+        let current2 = list2.head;
+        while (current1 && current2) {
+            if (current1.data < current2.data) {
+                mergedList.append(current1.data);
+                current1 = current1.next;
+            } else {
+                mergedList.append(current2.data);
+                current2 = current2.next;
+            }
+        }
+        while (current1) {
+            mergedList.append(current1.data);
+            current1 = current1.next;
+        }
+        while (current2) {
+            mergedList.append(current2.data);
+            current2 = current2.next;
+        }
+        return mergedList;
+    }
 }
 
-// Test the code
+// Test the code for merging two sorted linked lists
 const list1 = new DoublyLinkedList();
 list1.append(1);
 list1.append(3);
@@ -213,8 +141,9 @@ list2.append(4);
 list2.append(6);
 
 const mergedList = DoublyLinkedList.mergeTwoSortedLists(list1, list2);
-mergedList.printForward();
+mergedList.printForward(); // Should print: null <-> 1 <-> 2 <-> 3 <-> 4 <-> 5 <-> 6 <-> null
 
+// Test the code for deleting the N-th node from the end
 const list3 = new DoublyLinkedList();
 list3.append(1);
 list3.append(2);
@@ -223,4 +152,4 @@ list3.append(4);
 list3.append(5);
 
 list3.deleteNthFromEnd(2);
-list3.printForward();
+list3.printForward(); // Should print: null <-> 1 <-> 2 <-> 3 <-> 5 <-> null
